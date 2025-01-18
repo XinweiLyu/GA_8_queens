97866e0a972f50a4f5a922429cf51f62c9b46ae6
XinweiLyu <lxw491106884@gmail.com>
2025-01-18 16:18:38 -0500

    update

--- initialization.py
+++ initialization.py
@@ -4,15 +4,22 @@ My collection of initialization methods for different representations
 """
 
 #imports
+import numpy
 
 
-def permutation (pop_size, genome_length):
-    """initialize a population of permutation"""
+def permutation (pop_size, chrom_length):
+    """initialize a population of permutation
+        :return retuan a list(pop size number) of permutation
+    """
 
     population = []
     # student code begin
-
+    for i in range(pop_size):
+        # 生成一个随机排列，表示棋盘上的皇后位置
+        population.append(numpy.random.permutation(chrom_length))
     #student code end
     
     return population                     
 
+if __name__ == "__main__":
+    print(permutation(10, 8))
